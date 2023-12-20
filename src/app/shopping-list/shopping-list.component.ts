6474bda3f30f54c15cec600df0a8e2945c4dbf96
Kundan <kundan.k1988@gmail.com>
2023-12-20 23:23:08 +0530

    Data Binding

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  name = 'John Smith'
+  addToCart: any = 0
+  product = {
+    name: 'Iphone 14 plus',
+    price: 999,
+    color: 'Light Blue',
+    discount: 10,
+    instock: 1,
+    Pimage: '/assets/images/Iphone14plus.jpg'
+  }
+
+  getdiscountedprice(){
+   return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+
+  onNameChange(event: any){
+    this.name = event.target.value
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--
+    }
+  }
+
+  increamentCartValue(){
+    if(this.addToCart < this.product.instock){
+      this.addToCart++
+    }
+  }
 
 }
